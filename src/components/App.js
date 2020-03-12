6b07d568a30a93a9f335e82e54e91ee5b7f8935a
ghalibgendut <ghalibgendut@gmail.com>
2020-03-12 10:57:16 +0700

    navbar routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import {Route, BrowserRouter} from 'react-router-dom';
+
 
 // Components
 import Header from './Header';
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
-
+import { Form } from 'reactstrap';
 
 
 
@@ -13,9 +15,14 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-                <Header/>
-            </div>   
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/login" component={Login} />
+                </div>
+            </BrowserRouter>   
         )
     }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,34 +28,18 @@ class Header extends Component {
             <div>
                 {/* Navbar */}
                 <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    <NavbarBrand href="/">Home</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="mr-auto" navbar>
                             <NavItem>
-                                <NavLink href="/components/">Components</NavLink>
+                                <NavLink href="/register">Register</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                                <NavLink href="/login">Login</NavLink>
                             </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Options
-                                </DropdownToggle>
-                                <DropdownMenu right>
-                                    <DropdownItem>
-                                    Option 1
-                                    </DropdownItem>
-                                    <DropdownItem>
-                                    Option 2
-                                    </DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem>
-                                    Reset
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
                         </Nav>
+
                     </Collapse>
                 </Navbar>
             </div>
