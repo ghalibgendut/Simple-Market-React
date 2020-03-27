6e8b05691ab950829632ed82bdaa9dd46a27e8ab
ghalibgendut <ghalibgendut@gmail.com>
2020-03-27 10:53:50 +0700

    Redirect after Login to home

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,9 @@ import axios from 'axios';
 import {onLoginUser} from '../actions/index_actions';
 import { connect } from 'react-redux';
 
+// Akan me-redirect ke alamat tertentu
+import {Redirect} from 'react-router-dom';
+
 class Login extends Component {
 
     onButtonLogin = () => {
@@ -40,38 +43,84 @@ class Login extends Component {
 
 
     render() {
-        return (
-            <div>
-                <div className="container-fluid">
-                <div className="row">
-                    <div className="col-5 mx-auto mt-5 card">
-                        <div className="card-body">
-                            <div className="border-bottom border-secondary card-title text-center">
-                                <h1>Login</h1>
+        if (!this.props.uname) { // Juka Belum login 
+            return (
+                <div>
+                    <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-5 mx-auto mt-5 card">
+                            <div className="card-body">
+                                <div className="border-bottom border-secondary card-title text-center">
+                                    <h1>Login</h1>
+                                </div>
+    
+                                <form className="form-group">
+                                    <div className="card-title">
+                                        <h4>Username</h4>
+                                    </div>
+                                    <input ref={(input) => {this.username = input }} type="text" className="form-control" />
+    
+                                    <div className="card-title">
+                                        <h4>Password</h4>
+                                    </div>
+                                    <input ref={(input) => {this.password = input }} type="password" className="form-control" />
+                                </form>
+    
+                                <button className="btn btn-primary btn-block" onClick={(this.onButtonLogin)} >Login</button>
+                                {/* GET */}
+                                {/* <button className="btn btn-success btn-block" onClick={(this.onGetClick)} >Get Data</button> */}
                             </div>
-
-                            <form className="form-group">
-                                <div className="card-title">
-                                    <h4>Username</h4>
-                                </div>
-                                <input ref={(input) => {this.username = input }} type="text" className="form-control" />
-
-                                <div className="card-title">
-                                    <h4>Password</h4>
-                                </div>
-                                <input ref={(input) => {this.password = input }} type="password" className="form-control" />
-                            </form>
-
-                            <button className="btn btn-primary btn-block" onClick={(this.onButtonLogin)} >Login</button>
-                            {/* GET */}
-                            {/* <button className="btn btn-success btn-block" onClick={(this.onGetClick)} >Get Data</button> */}
                         </div>
                     </div>
                 </div>
-            </div>
-            </div>
-        )
+                </div>
+            )
+        }
+        else {
+            return <Redirect to="/"/>
+        }
     }
 }
 
-export default connect(null, {onLoginUser})(Login)
+let mapStateToProps = (state) => {
+    return {
+        uname: state.auth.username
+    }
+}
+
+
+export default connect(mapStateToProps, {onLoginUser})(Login)
+
+
+
+// return (
+//     <div>
+//         <div className="container-fluid">
+//         <div className="row">
+//             <div className="col-5 mx-auto mt-5 card">
+//                 <div className="card-body">
+//                     <div className="border-bottom border-secondary card-title text-center">
+//                         <h1>Login</h1>
+//                     </div>
+
+//                     <form className="form-group">
+//                         <div className="card-title">
+//                             <h4>Username</h4>
+//                         </div>
+//                         <input ref={(input) => {this.username = input }} type="text" className="form-control" />
+
+//                         <div className="card-title">
+//                             <h4>Password</h4>
+//                         </div>
+//                         <input ref={(input) => {this.password = input }} type="password" className="form-control" />
+//                     </form>
+
+//                     <button className="btn btn-primary btn-block" onClick={(this.onButtonLogin)} >Login</button>
+//                     {/* GET */}
+//                     {/* <button className="btn btn-success btn-block" onClick={(this.onGetClick)} >Get Data</button> */}
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     </div>
+// )
